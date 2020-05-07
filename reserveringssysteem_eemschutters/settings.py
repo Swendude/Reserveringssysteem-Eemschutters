1b55f0879ea84d2b3689490614646026a654f601
Swen Mulderij <swenmulderij@gmail.com>
2020-05-07 23:04:47 +0200

    Fix for Heroku.

--- reserveringssysteem_eemschutters/settings.py
+++ reserveringssysteem_eemschutters/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -128,5 +129,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
