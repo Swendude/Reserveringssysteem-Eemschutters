daec5476a61835666ff74a4ed5c006280f2a74aa
Swen Mulderij <swenmulderij@gmail.com>
2020-05-07 17:15:20 +0200

    Validatie rond getal slots.

--- reserveringssysteem_eemschutters/reserveringen/models.py
+++ reserveringssysteem_eemschutters/reserveringen/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-
+from django.core.exceptions import ValidationError
 
 class Baan(models.Model):
     naam = models.CharField(max_length=50)
@@ -30,17 +30,26 @@ class Protocol(models.Model):
     prioriteit = models.IntegerField()
 
     def __str__(self):
-        return f"Protocol {self.prioriteit}"
+        return f'Protocol {self.prioriteit}'
 
     @property
     def aantal_slots(self):
+        """
+        Bereken het aantal slots als gevolg van dit protocol.
+        """
         tijd_open = datetime.timedelta(hours=self.sluit.hour - self.open.hour,
                                        minutes=self.sluit.minute - self.open.minute)
 
         return tijd_open/self.slot_duur
+    def clean(self):
+        """
+        Valideer dat het aantal slots een rond getal is.
+        """
+        if not self.aantal_slots.is_integer():
+            raise ValidationError(f'Er zijn {self.aantal_slots} slots in dit protocol. Dit moet een rond getal zijn.')
 
     class Meta:
-        verbose_name_plural = "Protocollen"
+        verbose_name_plural = 'Protocollen'
 
 # class Slot(models.Model):
 #     datum = models.DateField()
