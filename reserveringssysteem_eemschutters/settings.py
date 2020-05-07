c73cd9ac2521f5ff71636ad22cb16be35436a404
Swen Mulderij <swenmulderij@gmail.com>
2020-05-08 00:49:38 +0200

    Fixed statics.

--- reserveringssysteem_eemschutters/settings.py
+++ reserveringssysteem_eemschutters/settings.py
@@ -128,9 +128,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
