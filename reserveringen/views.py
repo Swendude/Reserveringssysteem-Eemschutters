6916e5c4caf33506d8b56c4f8c57cb0e9c6d4d7c
copebo <coen@copebo.nl>
2020-05-10 18:11:14 +0200

    Overzicht van reserveringen gemaakt.

--- reserveringen/views.py
+++ reserveringen/views.py
@@ -33,6 +33,68 @@ def mijn_reserveringen(request):
 
 
 @login_required(login_url='/login/')
+def overzicht(request):
+    
+    # Determine our date based on an optional GET parameter 'next' that offsets to the next available day
+    view_date = timezone.now()
+    # Set our time to UTC 12:00:00, this should work with the math we do in `next_datetime_with_weekdays`
+    # without being bothered with timezone. I think we can handle timezones up to UTC +/- 12(ish).
+    view_date = view_date + datetime.timedelta(days=0,
+                                               hours=12 - view_date.time().hour,
+                                               minutes=-view_date.time().minute,
+                                               seconds=-view_date.time().second)
+
+    dagkeuze = 0
+
+    schietdagen = Schietdag.objects.order_by('dag')
+    schietdagen = list(map(lambda schietdag: (next_datetime_with_weekday(
+        view_date, schietdag.dag), schietdag), schietdagen))
+    schietdagen.sort(key=lambda t: (t[0] - view_date).total_seconds())
+
+    if dagkeuze > len(schietdagen) - 1:
+        dagkeuze = len(schietdagen)
+    gekozen_schietdag_datum, gekozen_schietdag = schietdagen[dagkeuze]
+    slot_tijden = gekozen_schietdag.slot_tijden()
+    banen = Baan.objects.all()
+
+    slots_per_baan = {}
+    for baan in banen:
+        slots_per_baan[baan] = []
+        for slot_tijd in slot_tijden:
+            status = "Vrij"
+            slot_start = timezone.make_aware(
+                datetime.datetime.combine(gekozen_schietdag_datum, slot_tijd[0]))
+            slot_eind = timezone.make_aware(datetime.datetime.combine(
+                gekozen_schietdag_datum, slot_tijd[1]))
+            reservering = Reservering.objects.filter(baan=baan,
+                                                     schietdag=gekozen_schietdag,
+                                                     start=slot_start,
+                                                     eind=slot_eind)
+            zelf = False
+            if reservering:
+                status = "Bezet"
+                if reservering[0].gebruiker == request.user:
+                    zelf=True
+            slot_form = ReserveringForm(initial={
+                'start':slot_start,
+                'eind':slot_eind,
+                'baan':baan.pk,
+                'schietdag':gekozen_schietdag.pk})
+            slots_per_baan[baan].append(
+                Slot(gekozen_schietdag_datum, slot_tijd[0], slot_tijd[1], baan, status, slot_form, zelf))
+    
+    return render(request, 'reserveringen/reserveringen_overzicht.html', {'view_date': view_date,
+                                                                'schietdagen': schietdagen,
+                                                                'gekozen_schietdag_datum': gekozen_schietdag_datum,
+                                                                'banen': banen,
+                                                                'slot_tijden': slot_tijden,
+                                                                'slots_per_baan': slots_per_baan,
+                                                                'choice': dagkeuze})
+
+    # return render(request, 'reserveringen/reserveringen_overzicht.html', {})
+
+
+@login_required(login_url='/login/')
 def reserveringen(request):
     if request.method == 'POST':
         # Create a reservation
@@ -105,3 +167,4 @@ def reserveringen(request):
                                                                 'slot_tijden': slot_tijden,
                                                                 'slots_per_baan': slots_per_baan,
                                                                 'choice': dagkeuze})
+
