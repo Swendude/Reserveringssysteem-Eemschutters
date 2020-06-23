47de9a80d2ac690e11dca822def22a50d2c4302e
Swen Mulderij <swenmulderij@gmail.com>
2020-06-23 22:43:29 +0200

    Reserveringen exporteren.

--- reserveringen/admin.py
+++ reserveringen/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 from .models import Baan, Schietdag, Reservering, SiteConfiguration, NieuwsBericht
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User  
+from django.contrib.auth.models import User
 from django import forms
 from django.urls import path
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.utils import timezone
 from collections import Set
 import csv
 from io import StringIO
+from django.template import loader
+
 
 class SchietdagAdmin(admin.ModelAdmin):
     fields = ['dag',
@@ -37,27 +41,50 @@ class SchietdagAdmin(admin.ModelAdmin):
 
         context.update(extra)
         return super(SchietdagAdmin, self).render_change_form(request,
-                                                           context, *args, **kwargs)
+                                                              context, *args, **kwargs)
 
 
 class ReserveringAdmin(admin.ModelAdmin):
     list_display = ['gebruiker', 'start', 'eind', 'baan']
     list_filter = ['gebruiker', 'start']
+    change_list_template = "reserveringen/reserveringen_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('export-csv/', self.export_csv),
+        ]
+        return my_urls + urls
+
+    def export_csv(self, request):
+        if request.method == "GET":
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = f'attachment; filename="reserveringen_{timezone.now()}.csv"'
+            
+            reserveringen = Reservering.objects.all()
+            
+            t = loader.get_template('reserveringen_csv.txt')
+            c = {'reserveringen': reserveringen}
+            response.write(t.render(c))
+            
+            return response
+
 
 class CsvImportForm(forms.Form):
     csv_file = forms.FileField()
 
-class CustomUserAdmin(UserAdmin):
-     change_list_template = "reserveringen/user_changelist.html"
 
-     def get_urls(self):
+class CustomUserAdmin(UserAdmin):
+    change_list_template = "reserveringen/user_changelist.html"
+
+    def get_urls(self):
         urls = super().get_urls()
         my_urls = [
             path('import-csv/', self.import_csv),
         ]
         return my_urls + urls
 
-     def import_csv(self, request):
+    def import_csv(self, request):
         if request.method == "POST":
             csv_file = request.FILES["csv_file"]
             csv_file = StringIO(csv_file.read().decode())
@@ -71,6 +98,7 @@ class CustomUserAdmin(UserAdmin):
         return render(
             request, "reserveringen/csv_form.html", payload
         )
+
 
 admin.site.register(Baan)
 admin.site.register(NieuwsBericht)
